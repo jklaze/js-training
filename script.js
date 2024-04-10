342623bc5106edf297060f0399e210212307c294
Bogdan Petrut <bogdanpetrut.319@gmail.com>
2024-04-10 13:13:46 +0200

    Refactor player selection and add singleRound function in script.js

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
+let player_selection = 'ROCK';
+let computer_selection = computerPlay();
+
 function computerPlay() {
-    let choices = ['rock', 'paper', 'scissors'];
+    let choices = ['ROCK', 'PAPER', 'SCISSORS'];
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
-console.log(computerPlay());
+function singleRound(player_selection, computer_selection) {
+    if (player_selection === computer_selection) {
+        return 'It\'s a tie!';
+    } else if (
+        player_selection === 'ROCK' && computer_selection === 'SCISSORS' ||
+        player_selection === 'PAPER' && computer_selection === 'ROCK' ||
+        player_selection === 'SCISSORS' && computer_selection === 'PAPER')
+    {
+        return 'You win!';
+    } else {
+        return 'You lose!';
+    }
+}
+
+console.log(
+    `You played ${player_selection} and Brankonator played ${computer_selection}... \n`
+    + singleRound(player_selection, computer_selection)
+);
