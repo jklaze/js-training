1d22078d9d50a7989e35aba6815838098cc544e4
Bogdan Petrut <bogdanpetrut.319@gmail.com>
2024-04-10 14:27:46 +0200

    Refactor game logic and add user interaction in script.js

--- script.js
+++ script.js
@@ -1,5 +1,56 @@
-let player_selection = 'ROCK';
-let computer_selection = computerPlay();
+function game() {
+    let player_score = 0;
+    let computer_score = 0;
+
+    for (let i = 0; i < 5; i++) {
+        let player_selection = playerPlay();
+        let computer_selection = computerPlay();
+        let result = singleRound(player_selection, computer_selection);
+        let result_message = '';
+
+        if (result === 'WIN') {
+            player_score++;
+            result_message = `You win! ${player_selection} beats ${computer_selection}`;
+        } else if (result === 'LOSE') {
+            computer_score++;
+            result_message = `You lose! ${computer_selection} beats ${player_selection}`;
+        } else {
+            result_message = `It's a tie! We both chose ${player_selection}`;
+        }
+
+        console.log(`
+            Round ${i + 1} \n
+            ${result_message} \n
+                You ${player_score} - ${computer_score} Brankonator \n`
+            );
+
+        if (player_score === 3 || computer_score === 3) {
+            break;
+        }
+    }
+
+    if (player_score > computer_score) {
+        return `You won the game! Finally, a human has beaten me...`;
+    } else if (player_score < computer_score) {
+        return `You lost the game! I am the ultimate AI!`;
+    } else {
+        if (confirm("It's a tie! I knew you couldn't beat me! But I feel generous... Do you want another chance?")) {
+            return game();
+        } else {
+            alert("You can't beat me! Goodbye!");
+        }
+    }
+}
+
+function playerPlay() {
+    let player_input = prompt('Enter your choice: ROCK, PAPER, or SCISSORS').toUpperCase();
+    if (player_input === 'ROCK' || player_input === 'PAPER' || player_input === 'SCISSORS') {
+        return player_input;
+    } else {
+        console.log("That's not even a choice! What a silly human... \n");
+        return playerPlay();
+    }
+}
 
 function computerPlay() {
     let choices = ['ROCK', 'PAPER', 'SCISSORS'];
@@ -14,13 +65,17 @@ function singleRound(player_selection, computer_selection) {
         player_selection === 'PAPER' && computer_selection === 'ROCK' ||
         player_selection === 'SCISSORS' && computer_selection === 'PAPER')
     {
-        return 'You win!';
+        return 'WIN';
     } else {
-        return 'You lose!';
+        return 'LOSE';
     }
 }
 
-console.log(
-    `You played ${player_selection} and Brankonator played ${computer_selection}... \n`
-    + singleRound(player_selection, computer_selection)
-);
+alert("I am Brankonator, the most powerful AI in the world! I am going to take over the world "
+    + "with my unbeatable Rock, Paper, Scissors game! \n");
+alert("A brain as powerful as mine can't be beaten by mere humans! \n");
+if(confirm("Do you dare to challenge me?")) {
+    console.log(game());
+} else {
+    alert("You are not ready to face me! Goodbye!");
+}
